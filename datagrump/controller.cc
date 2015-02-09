afc38519ed7484ff2d5d38288139951c323c1d1e
Keith Winstein <keithw@mit.edu>
2015-02-09 14:52:39 -0800

    Add initial contest runner (and reduce default window)

--- datagrump/controller.cc
+++ datagrump/controller.cc
@@ -14,7 +14,7 @@ Controller::Controller( const bool debug )
 unsigned int Controller::window_size( void )
 {
   /* Default: fixed window size of 100 outstanding datagrams */
-  unsigned int the_window_size = 100;
+  unsigned int the_window_size = 50;
 
   if ( debug_ ) {
     cerr << "At time " << timestamp_ms()
